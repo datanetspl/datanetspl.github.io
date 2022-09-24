469b4c7752d76949028242bbecdf7b51d7c2f02a
utkala mohanty <utkala@gmail.com>
2022-09-24 14:35:42 +0800

    changes

---
+++ CRMTracking/data.js
@@ -0,0 +1,37 @@
+var data= [
+ {
+   "Name": "Garrett Nixon",
+   "Position": "Sys Architect",
+   "Location": "Chicago",
+   "Value": "$320,000 ",
+   "Description": "Person A"
+ },
+ {
+   "Name": "Ashton Winters",
+   "Position": "Junior Tech",
+   "Location": "Los Angeles",
+   "Value": "$190,000 ",
+   "Description": "Person B"
+ },
+ {
+   "Name": "Brielle Cox",
+   "Position": "JS Specialist",
+   "Location": "San Francisco",
+   "Value": "$430,000 ",
+   "Description": "Person C"
+ },
+ {
+   "Name": "John Williamson",
+   "Position": "Sales",
+   "Location": "Austin",
+   "Value": "$510,000 ",
+   "Description": "Person D"
+ },
+ {
+   "Name": "Ron Cheng",
+   "Position": "Marketing",
+   "Location": "New York",
+   "Value": "$620,000 ",
+   "Description": "Person E"
+ }
+]
